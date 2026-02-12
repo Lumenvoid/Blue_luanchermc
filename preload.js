387cd3910104b24e51349e48fae6b7b9967807bf
Lumenvoid <9s78zt4kmj@privaterelay.appleid.com>
2026-02-12 18:54:24 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   startMicrosoftAuth: () => ipcRenderer.invoke('start-microsoft-auth'),
   pollForToken: (deviceCode, interval) => ipcRenderer.invoke('poll-for-token', deviceCode, interval),
   launchMinecraft: (version, authData) => ipcRenderer.invoke('launch-minecraft', version, authData),
-  uploadSkin: (skinPath, authToken) => ipcRenderer.invoke('upload-skin', skinPath, authToken),
-  openExternal: (url) => shell.openExternal(url)
+  uploadSkin: (skinDataUrl, authToken) => ipcRenderer.invoke('upload-skin', skinDataUrl, authToken),
+  openExternal: (url) => shell.openExternal(url),
+  onLaunchStatus: (callback) => ipcRenderer.on('launch-status', (event, status) => callback(status))
 });
