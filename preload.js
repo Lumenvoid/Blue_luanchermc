25351cb3d90cf6f4c03258c5a3611b186c13c985
Lumenvoid <9s78zt4kmj@privaterelay.appleid.com>
2026-02-12 16:38:58 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer, shell } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   startMicrosoftAuth: () => ipcRenderer.invoke('start-microsoft-auth'),
+  pollForToken: (deviceCode, interval) => ipcRenderer.invoke('poll-for-token', deviceCode, interval),
   openExternal: (url) => shell.openExternal(url)
 });
